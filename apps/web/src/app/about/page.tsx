47739ae592f53cfdc15e543fca1135648f8b915b
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-13 21:24:46 +0530

    feat(about): remove the work process section from the about page

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -1,29 +1,28 @@
-import { Navbar } from "@layout/Navbar";
-import { Footer } from "@layout/Footer";
-import { AboutHero } from "@/sections/about/AboutHero";
-import { AboutOverview } from "@/sections/about/AboutOverview";
-import { AboutMissionValues } from "@/sections/about/AboutMissionValues";
-import { AboutJourney } from "@/sections/about/AboutJourney";
-import { AboutDivisions } from "@/sections/about/AboutDivisions";
-import { AboutCertifications } from "@/sections/about/AboutCertifications";
-import { AboutStats } from "@/sections/about/AboutStats";
-import { AboutProcess } from "@/sections/about/AboutProcess";
-import { AboutClients } from "@/sections/about/AboutClients";
-import { AboutSafetySustainability } from "@/sections/about/AboutSafetySustainability";
 import { AboutCTA } from "@/sections/about/AboutCTA";
+import { AboutCertifications } from "@/sections/about/AboutCertifications";
+import { AboutClients } from "@/sections/about/AboutClients";
+import { AboutDivisions } from "@/sections/about/AboutDivisions";
+import { AboutHero } from "@/sections/about/AboutHero";
+import { AboutJourney } from "@/sections/about/AboutJourney";
+import { AboutMissionValues } from "@/sections/about/AboutMissionValues";
+import { AboutOverview } from "@/sections/about/AboutOverview";
+import { AboutSafetySustainability } from "@/sections/about/AboutSafetySustainability";
+import { AboutStats } from "@/sections/about/AboutStats";
+
+import { Footer } from "@layout/Footer";
+import { Navbar } from "@layout/Navbar";
 
 export default function AboutPage() {
   return (
     <main className="relative bg-white">
       <Navbar />
-      
+
       {/* 10-Step Premium About Us Experience */}
       <AboutHero />
       <AboutOverview />
       <AboutMissionValues />
       <AboutJourney />
       <AboutStats />
-      <AboutProcess />
       <AboutDivisions />
       <AboutCertifications />
       <AboutClients />
