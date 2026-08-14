024c0d8e84a8fc33112480dd4adef489e5990d9a
fitness <swalihhafiz332@gmail.com>
2026-08-14 15:46:33 +0300

    feat(auth): add AdminUser and AdminSession models

---
+++ apps/web/src/lib/auth/constants.ts
@@ -0,0 +1,4 @@
+export const ADMIN_COOKIE = "rvcc_admin_session";
+export const ADMIN_SESSION_MS = 8 * 60 * 60 * 1000;
+export const MAX_FAILED_ATTEMPTS = 5;
+export const LOCKOUT_MS = 15 * 60 * 1000;
