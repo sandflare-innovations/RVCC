7fc57416d5b1572a82e08975d192d5c61abfb3aa
ajmal faris <ajmalfaris.me@gmail.com>
2026-08-29 17:43:10 +0530

    refactor(web): add centralized barrel exports for sections and types

---
+++ apps/web/src/sections/index.ts
@@ -0,0 +1,54 @@
+// Home
+export * from "./home/Hero";
+export * from "./home/AboutUs";
+export * from "./home/Services";
+export * from "./home/Projects";
+export * from "./home/MajorProject";
+export * from "./home/OurWorks";
+export * from "./home/CSRSection";
+export * from "./home/Leaders";
+export * from "./home/NewsAndEvents";
+export * from "./home/ScrollingText";
+
+// About
+export * from "./about/AboutHero";
+export * from "./about/AboutOverview";
+export * from "./about/AboutMissionValues";
+export * from "./about/AboutJourney";
+export * from "./about/AboutDivisions";
+export * from "./about/AboutStats";
+export * from "./about/AboutSafetySustainability";
+export * from "./about/AboutCertifications";
+export * from "./about/AboutClients";
+export * from "./about/AboutCTA";
+
+// Careers
+export * from "./careers/CareerHero";
+export * from "./careers/CareerList";
+
+// Projects
+export * from "./projects/ProjectHero";
+export * from "./projects/ProjectList";
+export * from "./projects/ProjectFilters";
+export * from "./projects/ProjectMetrics";
+
+// Services
+export * from "./services/ServicesHero";
+export * from "./services/ServicesGrid";
+
+// Clients
+export * from "./clients/ClientsHero";
+export * from "./clients/ClientsGrid";
+
+// Documents
+export * from "./documents/DocumentHero";
+export * from "./documents/DocumentList";
+export * from "./documents/FlipbookReader";
+
+// Gallery
+export * from "./gallary/GallaryHero";
+export * from "./gallary/GallaryCollections";
+
+// Quality
+export * from "./quality/QualityHero";
+export * from "./quality/QualityContent";
--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -1,3 +1,5 @@
 export * from "./home";
 export * from "./projects";
 export * from "./company";
+export * from "./careers";
+
