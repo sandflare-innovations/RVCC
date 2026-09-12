4cb218190361a6ddb9d4d08f643ef84749b4fc89
ajmal faris <ajmalfaris.me@gmail.com>
2026-09-12 06:23:19 +0530

    feat(web): migrate quality certificates to cloudflare r2 file manager

--- apps/web/src/data/home/csr.ts
+++ apps/web/src/data/home/csr.ts
@@ -6,22 +6,22 @@ export const certificates: Certificate[] = [
   {
     name: "ISO 9001",
     code: "ISO - 9001 - 2008",
-    image: "/images/certificate/tuv.webp",
+    image: "https://pub-70b8c21f306842d3bbeab4d1d19319e1.r2.dev/file-manager/certificates/iso-9001-tuv-01fd.webp",
   },
   {
     name: "ISO 14001",
     code: "ISO - 14001 - 2004",
-    image: "/images/certificate/jas-anz-14001.webp",
+    image: "https://pub-70b8c21f306842d3bbeab4d1d19319e1.r2.dev/file-manager/certificates/iso-14001-jas-anz-o0ed.webp",
   },
   {
     name: "OHSAS 18001",
     code: "OHSAS - 18001 - 2007",
-    image: "/images/certificate/jas-anz-18001.webp",
+    image: "https://pub-70b8c21f306842d3bbeab4d1d19319e1.r2.dev/file-manager/certificates/ohsas-18001-jas-anz-5co8.webp",
   },
   {
     name: "IAF",
     code: "Member of Multilateral",
-    image: "/images/certificate/iaf.webp",
+    image: "https://pub-70b8c21f306842d3bbeab4d1d19319e1.r2.dev/file-manager/certificates/iaf-multilateral-e0ul.webp",
   },
 ];
 
